a04f8e7e798431331a896f3e1efe8d129372a6ca
Rafael Santos Corrêa <101223032+Faelsc@users.noreply.github.com>
2023-12-01 14:11:28 -0300

    :sparkles: definindo imagens

--- slideShow.js
+++ slideShow.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const images = [
+    { 'id': '1', 'url':'./img/peakyblinders.jpg' },
+    { 'id': '2', 'url':'./img/theoffice.jpg' },
+    { 'id': '3', 'url':'./img/strangerthings.jpg' },
+    { 'id': '4', 'url':'./img/theboys.jpg' },
+    { 'id': '5', 'url':'./img/dark.jpg' },
+    { 'id': '6', 'url':'./img/simpsons.png' },
+]
